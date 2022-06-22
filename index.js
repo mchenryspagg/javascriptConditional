cfd65cb3fcd58597bc769f8a992d5918f8ac0227
Henry Okam <hnry_ugo@yahoo.com>
2022-06-22 16:09:48 +0100

    Updated

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 // Building a basic arithmetic calculator
 let number1 = parseInt(prompt("Enter First Number: "))
+let operation = prompt("Enter your preferred operator (* / - + ^): ")
 let  number2 = parseInt(prompt("Enter Second Number: "))
-let operation = prompt("Enter your preferred operator (* / - +): ")
 
 if (operation === "+") {
-    alert(number1 + number2); 
+    alert(number1 + "+" + number2 + "=" + parseInt(number1 + number2));
 }
-else if (operation === "-") {
-    alert(number1 - number2);
+else if (operation === "-") { 
+    alert(number1 + "-" + number2 + "=" + parseInt(number1 - number2));
 }
 else if (operation === "*") {
-    alert(number1 * number2);
+    alert(number1 + "*" + number2 + "=" + parseInt(number1 * number2));
 }
 else if (operation === "/") {
-    alert(number1 / number2);
+    alert(number1 + "/" + number2 + "=" + parseInt(number1 / number2));
+}
+else if (operation === "^") {
+    alert(number1 + "^" + number2 + "=" + parseInt(number1 ** number2));
 }
 else alert("Re-enter correct parameter");
